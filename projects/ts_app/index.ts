abd5f9fe9b9529a9e69e1b3e4bd9e03bc99159b3
KrauseStefan <stefan.krausekjaer@gmail.com>
2025-03-18 00:01:57 +0100

    Temp ts demo code

--- projects/ts_app/index.ts
+++ projects/ts_app/index.ts
@@ -1,9 +1,21 @@
 import Calculator from './calculator';
+import CalculatorMul from './calculator_mul';
+import * as express from 'express';
 
-const calculator: Calculator = new Calculator();
+const calculator = new Calculator();
+const calculatorMul = new CalculatorMul();
 const num1: number = 1;
 const num2: number = 2;
 
 const result = calculator.add(num1, num2);
-
 console.log(`${num1} + ${num2} = ${result}`);
+
+const result2 = calculatorMul.mul(num1, num2);
+console.log(`${num1} * ${num2} = ${result}`);
+
+const app = express();
+
+const port = 10005;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
